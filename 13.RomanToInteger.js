6ac4490017ce4fd9699c23526e9537255b925e4f
Pedro Melo <65244423+pedrohmelo@users.noreply.github.com>
2023-12-18 16:14:04 -0300

    Add files via upload

---
+++ 13.RomanToInteger.js
@@ -0,0 +1,29 @@
+var romanToInt = function(s) {
+    // Create a map to store the values of Roman numerals
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+  
+    let result = 0;
+  
+    for(let i = 0; i < s.length; i++){
+      if(romanValues[s[i]] < romanValues[s[i+1]]){
+        result -= romanValues[s[i]];
+       } else{
+        result += romanValues[s[i]]
+       }
+    }
+  
+    return result
+  
+  };
+  
+  console.log(romanToInt("III"))
+  console.log(romanToInt("LVIII"))
+  console.log(romanToInt("MCMXCIV"))
