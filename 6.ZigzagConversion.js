b697853ace7b4b4be2b2ab962f9bcbd24d0af40e
Pedro Melo <65244423+pedrohmelo@users.noreply.github.com>
2023-12-26 17:01:46 -0300

    Update 6.ZigzagConversion.js

--- 6.ZigzagConversion.js
+++ 6.ZigzagConversion.js
@@ -1,15 +1,26 @@
 let s = "PAYPALISHIRING";
 let numRows = 3;
 let newColumn = "";  // Initialize as an empty string
-let aux = 0;
+let characterPos = 0;
+let rowCount = 1;
+
+//desce na matriz até chegar na posição numRows
+//[3]
+//vai pra direita e sobe uma casa -> [2]
+//coloca valor
+//verifica se está na linha 1, se não estiver vai pra direita e sobe uma casa -> [1]
+
 
 var convert = function(s, numRows) {
-  while (aux < numRows) {
-    newColumn += s[aux];  // Use += to concatenate characters
-    aux++;
-  }
-   
-  if(aux == numRows){
+  for(const element of s){
+    newColumn += s[characterPos]
+    characterPos++;
+    rowCount++;
+
+    if(rowCount == numRows + 1){
+      return newColumn
+      //
+    }
     
   }
 
@@ -17,7 +28,6 @@ var convert = function(s, numRows) {
 };
 
 console.log(convert(s, numRows));  // Output: "PAY"
-
 
 // P   A   H   N
 // A P L S I I G
